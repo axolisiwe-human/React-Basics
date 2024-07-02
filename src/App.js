94cd85027a3217ef8355e480eda377b94243f8ee
Human <humanbakishi@gmail.com>
2024-07-02 03:03:58 -0700

    React basic task

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import QRCODE from './qrcode.png'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='image'>
+       <img src={QRCODE} alt="QR code" />
+
+      </div>
+      <div className='paragraph1'>
+        <b><p>Improve your front-end skills by building projects</p></b>
+      </div>
+      <div className='paragraph2'>
+        <p>Scan the QR code to visit Front-end Mentor and take your coding skills to the next level</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
